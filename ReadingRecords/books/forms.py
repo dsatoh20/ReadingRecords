46801379875a5387fc7d33c150083bb7d651f9a0
dsatoh20 <dsatoh@GQ2DPW2.localdomain>
2024-01-03 02:23:42 +0900

    first-demoを共有

---
+++ ReadingRecords/books/forms.py
@@ -0,0 +1,97 @@
+from django import forms
+from.models import BookRecord, Group, Friend, Good
+from django.contrib.auth.models import User
+
+class BookRecordForm(forms.ModelForm):
+    class Meta:
+        model = BookRecord
+        fields = ['owner', 'group', 'title']
+        
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['owner', 'title']
+        
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['owner', 'user', 'group']
+        
+class GoodForm(forms.ModelForm):
+    class Meta:
+        model = Good
+        fields = ['owner', 'bookrecord']
+        
+class GroupCheckForm(forms.Form):
+    def __init__(self, user, *args, **kwargs):
+        super(GroupCheckForm, self).__init__(*args, **kwargs)
+        public = User.objects.filter(username='public').first()
+        self.fields['groups'] = forms.MultipleChoiceField(
+            choices=[(item.title, item.title) for item in \
+                Group.objects.filter(owner__in=[user, public])],
+            widget=forms.CheckboxSelectMultiple(),
+        )
+
+class GroupSelectForm(forms.Form):
+    def __init__(self, user, *args, **kwargs):
+        super(GroupSelectForm, self).__init__(*args, **kwargs)
+        self.fields['groups'] = forms.ChoiceField(
+            choices=[('-', '-')] + [(item.title, item.title) \
+                for item in Group.objects.filter(owner=user)],\
+            widget=forms.Select(attrs={'class':'form-control'}),
+        )
+        
+class FriendsForm(forms.Form):
+    def __init__(self, user, friends=[], vals=[], *args, **kwargs):
+        super(FriendsForm, self).__init__(*args, **kwargs)
+        self.fields['friends'] = forms.MultipleChoiceField(\
+            choices=[(item.user, item.user) for item in friends], \
+                widget=forms.CheckboxSelectMultiple(), \
+                    initial=vals,
+        )
+
+class CreateGroupForm(forms.Form):
+    group_name = forms.CharField(max_length=50, \
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+class PostForm(forms.Form):
+    title = forms.CharField(max_length=100, \
+        widget=forms.Textarea(attrs={'class': 'form-control',
+                                     'rows': 1,
+                                     'placeholder': "俺か、俺以外か　ローランドという生き方"}))
+    first_author = forms.CharField(max_length=100, \
+        widget=forms.Textarea(attrs={'class': 'form-control',
+                                     'rows':1,
+                                     'placeholder': "ROLAND"}))
+    pub_year = forms.IntegerField(max_value=3000, \
+        widget=forms.Textarea(attrs={'class': 'form-control',
+                                     'rows':1,
+                                     'placeholder': "2019"}))
+    genre = forms.CharField(max_length=100, \
+        widget=forms.Textarea(attrs={'class': 'form-control',
+                                     'rows':1,
+                                     'placeholder': '自己啓発'}))
+    score = forms.IntegerField(max_value=10, min_value=1, \
+        widget=forms.Textarea(attrs={'class': 'form-control',
+                                     'rows':1,
+                                     'placeholder': "1~10の10段階評価"}))
+    summary = forms.CharField(max_length=500, \
+        widget=forms.Textarea(attrs={'class': 'form-control',
+                                     'rows':4,
+                                     'placeholder': "500字以内で入力してください。"}))
+    report = forms.CharField(max_length=5000, \
+        widget=forms.Textarea(attrs={'class': 'form-control',
+                                     'rows':10,
+                                     'placeholder': "5000字以内で入力してください。"}))
+    
+    
+    def __init__(self, user, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        public = User.objects.filter(username='public').first()
+        self.fields['group'] = forms.ChoiceField(
+            choices=[('-', '-')] + [(item.title, item.title)\
+                for item in Group.objects.filter(owner__in=[user, public])],
+            widget=forms.Select(attrs={'class':'form-control'}),
+        )
+        
+        
---
+++ ReadingRecords/books/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Create your models here.
+class Developer(models.Model): # 開発者のモデルクラス
+    name = models.CharField(max_length=100)
+    mail = models.EmailField(max_length=100)
+    gender = models.BooleanField()
+    # age = models.IntegerField(default=0)
+    birthday = models.DateField(blank=True, null=True)
+    
+    def __str__(self):
+        return '<Friend:id=' + str(self.id) + ',' + \
+            self.name + '>'
+
+
+class BookRecord(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,\
+        related_name='message_owner')
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    title = models.TextField(max_length=100)
+    first_author =models.TextField(max_length=100)
+    pub_year = models.IntegerField(default=0)
+    genre = models.TextField(max_length=100)
+    score = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    summary = models.TextField(max_length=500)
+    report = models.TextField(max_length=5000)
+    good_count = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.title) + '/' + str(self.first_author) + ' 著'
+    class Meta:
+        ordering = ('-pub_date',)
+        
+class Group(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, \
+        related_name='group_owner')
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return '<' + self.title + '(' + str(self.owner) + ')>'
+    
+class Friend(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, \
+        related_name='friend_owner')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.user) + '(group:"' + str(self.group) + '")'
+
+class Good(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, \
+        related_name='good_owner')
+    bookrecord = models.ForeignKey(BookRecord, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return 'good for "' + str(self.bookrecord) + '" (by ' + \
+            str(self.owner) + ')'
+
+# class NewsRecord(models.Model):
+# class CinemaRecord(models.Model):
+# class PaperRecord(models.Model):
+# class RakugoRecord(models.Model):
+# class MusicRecord(models.Model):
